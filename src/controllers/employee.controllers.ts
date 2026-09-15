cf30077d83bf58fe80708b7e4619c390a4c40b30
Ramiro <ramiroroman306@gmail.com>
2026-09-15 12:57:31 -0300

    Implementacion de los controladores

--- src/controllers/employee.controllers.ts
+++ src/controllers/employee.controllers.ts
@@ -1,17 +1,64 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
+import { EmployeeService } from "../services/employee.service.ts"
+export class EmployeeControllers {
 
-class EmployeeControllers {
+    constructor(private readonly employeeService: EmployeeService) { }
 
-    constructor() { }
-    createEmployee(req: Request, res: Response) { }
+    createEmployee = async (req: Request, res: Response): Promise<void> => {
+        try {
+            // El servicio hace su trabajo con el req.body
+            const newEmployee = await this.employeeService.createEmployee(req.body);
+            res.status(201).json({
+                status: "succes",
+                data: newEmployee
+            })
+        } catch (error: any) {
+            res.status(400).json({
+                error: error.message
+            })
+        };
+    }
 
-    findAllEmployees(req: Request, res: Response) { }
+    findAllEmployees = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const employees = await this.employeeService.findAllEmployees();
+            res.status(200).json(employees);
+        } catch (error: any) {
+            res.status(500).json({ error: "Error interno del servidor" });
+        }
+    }
 
-    findOneEmployeeById(req: Request, res: Response) { }
+    findOneEmployeeById = async (req: Request, res: Response): Promise<void> => {
+        try {
+            // Definir que el id vendra si o si como string
+            const id = req.params.id as string;
+            const employee = await this.employeeService.findOneEmployeeById(id);
+            res.status(200).json(employee);
+        } catch (error: any) {
+            res.status(404).json({ error: error.message })
+        }
+    }
 
-    updateEmployee(req: Request, res: Response) { }
+    updateEmployee = async (req: Request, res: Response): Promise<void> => {
+        try {
+            // Definir que el id vendra si o si como string
+            const id = req.params.id as string;
+            const updatedEmployee = await this.employeeService.updateEmployee(id, req.body);
+            res.status(200).json({ status: "success", updatedEmployee });
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+    }
 
-    deleteEmployee(req: Request, res: Response) { }
+    deleteEmployee = async (req: Request, res: Response): Promise<void> => {
+        try {
+            // Definir que el id vendra si o si como string
+            const id = req.params.id as string;
+            await this.employeeService.deleteEmployee(id);
+            res.status(200).json({ message: "Empleado eliminado correctamente" })
+        } catch (error: any) {
+            res.status(404).json({ error: error.message });
+        }
+    }
 }
 
-export default EmployeeControllers
