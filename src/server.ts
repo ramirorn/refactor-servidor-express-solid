9a570ea6f9730762ed6b3009a860c290ad0097c9
Ramiro <ramiroroman306@gmail.com>
2026-09-15 18:57:22 -0300

    Implementación de todas las capas en el servidor

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,57 @@
 import express, { Application } from "express"
+import "dotenv/config"
 
+// Todas las capas
+import { Database } from "./config/db.ts"
+import { EmployeeMongoRepository } from "./repository/employee.mongo.repository.ts"
+import { EmployeeService } from "./services/employee.service.ts"
+import { EmployeeControllers } from "./controllers/employee.controllers.ts"
+import { EmployeeRoutes } from "./routes/employee.routes.ts"
 
 class Server {
     private app: Application;
+    private port: string | number;
+    private db: Database;
 
     constructor() {
         this.app = express();
+        this.port = process.env.PORT ?? 3000;
+        this.db = new Database()
+        this.middlewares()
+        this.routes()
     }
 
-    dbConnect() { }
+    async dbConnect() {
+        await this.db.connect();
+    }
 
     middlewares() {
         this.app.use(express.json())
     }
 
-    routes() { }
+    routes() {
+        // Ensamblaje de dependencias
+        // Repositorio (BD)
+        const employeeRepository = new EmployeeMongoRepository();
+        // Servicio (Logica)
+        const employeeService = new EmployeeService(employeeRepository);
+        // Controlador (traductor)
+        const employeeController = new EmployeeControllers(employeeService);
+        // Rutas (endpoints)
+        const employeeRoutes = new EmployeeRoutes(employeeController);
+        // Ruta principal de la app
+        this.app.use("/api", employeeRoutes.router);
+    }
 
-    listen() { }
+    async listen() {
+        await this.dbConnect();
+
+        this.app.listen(this.port, () => {
+            console.log(`🚀 Servidor Express escuchando en http://localhost:${this.port}`)
+        })
+    }
 }
 
 const server = new Server()
 
-export default server;
+server.listen()
