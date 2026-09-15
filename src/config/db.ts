0883cb965f49d2c63b14c12bce13828524a7124e
Ramiro <ramiroroman306@gmail.com>
2026-09-15 13:07:32 -0300

    Implementacion de la conexion a la BD

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,21 @@
 import mongoose from "mongoose"
 import "dotenv/config"
 
-const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/employees_db';
+export class Database {
 
-export const connectDb = mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-        console.log('MongoDB conectado');
-        app.listen(PORT, () => {
-            console.log(`Servidor escuchando en http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('No se pudo conectar a MongoDB', error);
-        process.exit(1);
-    });
+    private readonly URI: string;
+
+    constructor() {
+        this.URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/employees_db';
+    }
+
+    public async connect(): Promise<void> {
+        try {
+            await mongoose.connect(this.URI);
+            console.log('🟢 Conectado a MongoDB con éxito');
+        } catch (error) {
+            console.error('🔴 Error al conectar a MongoDB:', error);
+            process.exit(1);
+        }
+    }
+}
